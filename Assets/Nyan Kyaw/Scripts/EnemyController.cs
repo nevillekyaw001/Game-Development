cc9fbe27d534d85cc9f53fab4a7a6e7b0b652172
Neville <103945243+nevillekyaw001@users.noreply.github.com>
2025-01-02 20:36:23 +0700

    Added Prototype elements
    - Added plater and its movement
    - Added coin gameobject and its animation
    - Added enemy and its movement

---
+++ Assets/Nyan Kyaw/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    public void MoveEnemy()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+    }
+}
---
+++ Assets/Nyan Kyaw/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 50f;
+
+    private Vector3 playerDirection;
+
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        rb.AddForce(playerDirection * playerSpeed);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected");
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #region Input System
+    public void OnMove(InputValue movementValue)
+    {
+        Vector2 inputVector = movementValue.Get<Vector2>();
+        playerDirection = new Vector3(inputVector.x, 0, inputVector.y);
+    }
+
+    #endregion
+}
